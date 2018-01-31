8b0816b3b98789371aec89d7860f269a31818739
M-Jsonzhang <34618124+zqy2046@users.noreply.github.com>
2018-01-31 20:31:55 +0800

    Update GoodsItem.js
    完善

--- cart/app/components/GoodsItem.js
+++ cart/app/components/GoodsItem.js
@@ -4,15 +4,24 @@ export default class GoodsItem extends Component {
   constructor(props) {
     super(props)
   }
+  handleClick(item,deleteItem,setAmount) {
+    if (item.amount === 1) {
+      if (confirm(`确定要删除${item.name}吗?`)) {
+        deleteItem(item.id)
+        return;
+      }
+    }
+    setAmount(item.id,item.amount-1)
+  }
   render() {
-    let {item,setAmount} = this.props;
+    let {item,setAmount,deleteItem} = this.props;
     return(
       <div className="goodsItem" style={{width:'100%',height: '50px'}}>
         <span className="id cell">{item.id}</span>
         <span className="name cell">{item.name}</span>
         <span className="price cell">{item.price}</span>
         <span className="amount cell">
-          <button onClick={() => {setAmount(item.id,item.amount -1)}}>-</button>
+          <button onClick={() => {this.handleClick(item,deleteItem,setAmount)}}>-</button>
             {item.amount}
           <button onClick={() => {setAmount(item.id,item.amount +1)}}>+</button>
         </span>
